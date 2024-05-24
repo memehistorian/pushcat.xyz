77809ba369fec3010810a6da6ec611a39fd86575
meloncatdev <meloncatdev@gmail.com>
2024-05-23 21:36:01 -0700

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+// noinspection ES6UnusedImports
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+// Custom bootstrap file
+import './bootstrap.js';
+
+// 3rd-party JS file(s)
+import * as bootstrap from 'bootstrap'
+
+// 3rd-party CSS file(s)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
+import './styles/app.css';
---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page', methods: ['GET', 'HEAD'])]
+    public function index(): Response
+    {
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+}
