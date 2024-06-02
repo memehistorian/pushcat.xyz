b834d35cf287eb7f98fc08d94726c328de3da7d3
meloncatdev <meloncatdev@gmail.com>
2024-06-02 04:14:00 -0700

    heroku updates 2.

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,9 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
 const app = startStimulusApp();
+
+// added to help heroku deployment. 
+const port = process.env.PORT || 3000
+
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
